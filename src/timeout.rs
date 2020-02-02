a32bb5cb5a8f710bb8f02a8447128e29815ccd7d
SOFe <sofe2038@gmail.com>
2020-02-03 01:46:19 +0800

    Fixed incorrect tokio timeout impl

--- src/timeout.rs
+++ src/timeout.rs
@@ -32,19 +32,10 @@ pub mod stdweb {
 pub mod tokio {
     use std::time::Duration;
 
-    use futures::channel::oneshot;
     use futures::future::{self, Either, Future};
 
-    pub fn delay(duration: Duration) -> oneshot::Receiver<()> {
-        let (send, recv) = oneshot::channel();
-        stdweb::web::set_timeout(
-            move || {
-                let _ = send.send(());
-            },
-            duration.as_millis() as u32,
-        );
-
-        recv
+    pub fn delay(duration: Duration) -> tokio::time::Delay {
+        tokio::time::delay_for(duration)
     }
 
     pub async fn timeout<T, F: Future<Output = T> + Unpin>(
