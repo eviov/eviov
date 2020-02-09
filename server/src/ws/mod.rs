233e32a50b6f1b6903b0059d2528881294ae3136
SOFe <sofe2038@gmail.com>
2020-02-09 19:48:58 +0800

    rustfmt

--- server/src/ws/mod.rs
+++ server/src/ws/mod.rs
@@ -18,12 +18,18 @@ pub async fn start<X: Plugin>(plugin: Arc<X>) -> io::Result<()> {
 }
 
 async fn entry<X: Plugin>(plugin: Arc<X>, stream: net::TcpStream, addr: SocketAddr) {
-    async fn inner<X: Plugin>(plugin: Arc<X>, stream: net::TcpStream, addr: SocketAddr) -> Result<(), String> {
-        let wss = tokio_tungstenite::accept_async(stream).await.map_err(|err| err.to_string())?;
+    async fn inner<X: Plugin>(
+        plugin: Arc<X>,
+        stream: net::TcpStream,
+        addr: SocketAddr,
+    ) -> Result<(), String> {
+        let wss = tokio_tungstenite::accept_async(stream)
+            .await
+            .map_err(|err| err.to_string())?;
         unimplemented!()
     }
 
-    if let Err(err) =  inner(plugin, stream, addr).await {
+    if let Err(err) = inner(plugin, stream, addr).await {
         log::error!("Error handling connection from {}: {}", addr, err);
     }
 }
