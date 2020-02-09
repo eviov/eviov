0c08447d422cf43f2862c52f86f7a915fb777afb
SOFe <sofe2038@gmail.com>
2020-02-09 20:09:39 +0800

    Documentation for `codegen` crate

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,4 +1,5 @@
 #![warn(
+    missing_docs,
     unused_results,
     unused_qualifications,
     variant_size_differences,
@@ -20,6 +21,8 @@
 )]
 #![cfg_attr(not(debug_assertions), deny(warnings, clippy::dbg_macro,))]
 
+//! Code generation crate for eviov
+
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
@@ -27,6 +30,41 @@ use proc_macro::TokenStream;
 #[path = "proto.rs"]
 mod proto_;
 
+/// Generates a protocol definition.
+///
+/// This macro generates hardcoded symbols like `Client`, `Proto` and `Server`.
+/// Each `codegen::proto!` should have its dedicated module.
+///
+/// # Syntax
+/// The macro block can start with inner attributes, which are going to be applied on the generated `Proto` struct.
+///
+/// The content then starts with the protocol name: `name = "xxx";`.
+///
+/// Then there can be any number of messages of queries, with the following format:
+/// ```text
+/// ITEM := OUTER_ATTRIBUTES* (MESSAGE | QUERY)
+///
+/// # The IDENT is the message name.
+/// # The FIELD_LIST contains the fields of the message.
+/// MESSAGE := DIRECTION "message" IDENT FIELD_LIST
+///
+/// # The IDENT is the query name. The request and response structs start with the query name,
+/// followed by "Request" or "Response".
+/// # The first FIELD_LIST contains the fields of the request.
+/// # The second FIELD_LIST contains the fields of the request.
+/// QUERY := DIRECTION "query" IDENT FIELD_LIST "->" FIELD_LIST
+///
+/// # DIRECTION indicates the *source* of the message or the *sender* of the query request
+/// (i.e. the *receiver* of the query *response*)
+/// DIRECTION := "client" | "server" | "mutual"
+///
+/// FIELD_LIST := "{" ( FIELD )* "}"
+///
+/// # The outer attributes are applied to the message struct, or the request struct for queries
+/// # The IDENT is the name of the field.
+/// # The TYPE is the type of the field.
+/// FIELD := OUTER_ATTRIBUTES* IDENT ":" TYPE ","
+/// ```
 #[proc_macro]
 pub fn proto(ts: TokenStream) -> TokenStream {
     proto_::main(ts.into())
